c6ebd5e23ed6861f23c10c33703ca3c277b3a649
Dada878 <xx0932399@gmail.com>
2024-01-14 18:03:43 +0800

    add font-awesome package

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faForward, faRotateRight } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faForward, faRotateRight);
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(createPinia())
 app.use(router)
