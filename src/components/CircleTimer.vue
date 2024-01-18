15ee90b5e3c93a1d7047b67d20db63815a68de68
Dada878 <xx0932399@gmail.com>
2024-01-19 01:53:49 +0800

    update coding style for formatTime function

--- src/components/CircleTimer.vue
+++ src/components/CircleTimer.vue
@@ -234,12 +234,8 @@ function startTimer() {
 function formatTime(time: number) {
   const minutes = Math.floor(time / 60)
   let seconds: number | string = time % 60
-  if (seconds < 10) {
-    seconds = `0${seconds}`
-  }
-  if (minutes < 10) {
-    return `0${minutes}:${seconds}`
-  }
+  if (seconds < 10) seconds = `0${seconds}`
+  if (minutes < 10) return `0${minutes}:${seconds}`
   return `${minutes}:${seconds}`
 }
 
